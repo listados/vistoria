49326406a828ff2ebc063a1dd2eddd44e1796f38
JuinEsp <franciscoanto@gmail.com>
2022-10-15 13:06:03 -0300

    Criado e add componente header-breadcrumb

---
+++ resources/views/components/header-breadcrumb.blade.php
@@ -0,0 +1,9 @@
+<section class="content-header">
+    <h1>
+        {{ $title}}
+        <small>{{ $subTitle }}</small>
+    </h1>
+    <ol class="breadcrumb">
+        {{ $links }}
+    </ol>
+</section>
---
+++ resources/views/proposal/cadastre-pf/index.blade.php
@@ -0,0 +1,38 @@
+@extends('adminlte::page')
+
+@section('title', 'Cadastro PF - Espindola Admin')
+
+@section('content_header')
+@component('components.header-breadcrumb')
+    @slot('title')
+        Cadastro Pessoa Física
+    @endslot
+    @slot('subTitle')
+        Todas os cadastros de fiadores Pessoa Física
+    @endslot
+    @slot('links')
+        <li><a href="#"><i class="fa fa-dashboard"></i> Escolha Azul</a></li>
+        <li class="active">Cadastro Pessoa Física</li>
+    @endslot
+@endcomponent
+@stop
+
+@section('content')
+
+<div class="box box-primary">
+    <h2>Fiador</h2>
+</div>
+
+
+@stop
+
+@section('css')
+<link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+{{-- 
+{{ Html::script('/js/manifest.js') }}
+{{ Html::script('/js/vendor.js') }} --}}
+{{ Html::script('js/proposal_pf.js') }}
+@stop
