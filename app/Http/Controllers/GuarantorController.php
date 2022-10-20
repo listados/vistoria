c69510811589b13ccb78a2b1afd27fe44b78c43a
JuinEsp <junior.oliveira@esp.ce.gov.br>
2022-10-20 16:11:25 -0300

    Enviando dados estatico e arquivos de css

--- app/Http/Controllers/GuarantorController.php
+++ app/Http/Controllers/GuarantorController.php
@@ -93,7 +93,12 @@ class GuarantorController extends Controller
      */
     public function getGuarantorDataTable()
     {
-        $guarantor = Guarantor::orderBy('guarantor_id', 'desc');
+        // $guarantor = Guarantor::orderBy('guarantor_id', 'desc');
+        $guarantor = [
+            ['date' => '2016-05-03', 'name' => 'Tom', 'state' => 'California', 'city' => 'Los Angeles', 'address' => 'No. 189, Grove St, Los Angeles', 'zip' => 'CA 90036', 'tag' => 'Home' ],
+            ['date' => '1984-11-05', 'name' => 'Junior', 'state' => 'New York', 'city' => 'Los Angeles', 'address' => 'No. 189, Grove St, Los Angeles', 'zip' => 'CA 90036', 'tag' => 'Home' ],
+            ['date' => '2016-05-03', 'name' => 'Paul', 'state' => 'California', 'city' => 'Los Angeles', 'address' => 'No. 189, Grove St, Los Angeles', 'zip' => 'CA 90036', 'tag' => 'Home' ]
+        ];
         return response()->json($guarantor);
     }
 
