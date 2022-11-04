d9521152f1db3ad1f9142396ad946fd50d55ab7d
JuinEsp <franciscoanto@gmail.com>
2022-11-04 00:27:26 -0300

    Add Ckeditor

---
+++ resources/assets/js/components/Survey/Content.vue
@@ -0,0 +1,49 @@
+<template lang="">
+    <div>
+        <ckeditor v-model="editorData" :config="editorConfig"></ckeditor>
+    </div>
+</template>
+<script>
+import VueCkeditor from 'vue-ckeditor2';    
+export default {
+    components: { VueCkeditor },
+    data() {
+        return {
+            content: '',
+            config: {
+                toolbar: [
+                ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript']
+                ],
+                height: 300
+            },
+            editorData: '<p>Content of the editor.</p>',
+            editorConfig: {
+                // The configuration of the editor.
+            }
+        }
+    },
+    methods: {
+        onBlur(evt) {
+        console.log(evt);
+        },
+        onFocus(evt) {
+        console.log(evt);
+        },
+        onContentDom(evt) {
+        console.log(evt);
+        },
+        onDialogDefinition(evt) {
+        console.log(evt);
+        },
+        onFileUploadRequest(evt) {
+        console.log(evt);
+        },
+        onFileUploadResponse(evt) {
+        console.log(evt);
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
