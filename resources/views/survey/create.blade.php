4e3441e591168e73d629f0cd30f06ec8ba9ce971
JuinEsp <franciscoanto@gmail.com>
2022-11-04 00:29:30 -0300

    Refatorado layout

--- resources/views/survey/create.blade.php
+++ resources/views/survey/create.blade.php
@@ -18,15 +18,14 @@
 @section('content')
 
 <!-- Main content -->
-<section class="content">
-	<div class="row">
+
+@section('content')
+    <div class="row">
 		<div class="col-md-12">
-			@include('message.message_general')
-		</div>
-	</div>
-	<div class="row">
-		<div class="box">
 			<div class="box box-primary">
+				<div class="box-header with-border">
+					<h3 class="box-title">Editar Vistoria</h3>
+				</div>
 				<div class="box-body">
 					<!-- ÁREA LOCADOR -->
 					<create-user 
@@ -34,23 +33,14 @@
 						:id-survey="{{ json_encode($id_survey) }}">
 					</create-user>
 					<!-- FIM ÁREA LOCADOR -->
-					<div class="container" >						
-						<div class='elementOccupant' id='div_1'></div>
-					</div>
-					<!-- FIM ÁREA LOCADOR -->
-					<!-- ÁREA LOCATÁRIO -->
 					<div class="col-md-12">
 						<hr>
-						{{-- <a href="#containerLocatario" class="add" id="" onclick="addLocatarioSurvey('elementLocatario', 'divLocatario', 'containerLocatario', 'removeLocatario' , 'occupant')" data-toggle="tooltip" data-placement="right" title="Adicionar novo locatário a vistoria"><i class="fa fa-plus-circle" aria-hidden="true"></i> Add Locatário</a> --}}
 					</div>
 					<!-- ÁREA LOCATÁRIO -->
 					<create-user 
 						:type-survey="{{ json_encode('Locatario') }}"
 						:id-survey="{{ json_encode($id_survey) }}">
 					</create-user>
-					<div class="containerLocatario" >
-						<div class='elementLocatario' id='divLocatario_1'></div>
-					</div>
 					<!--FIM ÁREA LOCATÁRIO-->
 
 					<!-- ÁREA FIADOR -->
@@ -61,14 +51,10 @@
 					:type-survey="{{ json_encode('Fiador') }}"
 					:id-survey="{{ json_encode($id_survey) }}">
 					</create-user>
-					<div class="containerFiador" >
-						<div class='elementFiador' id='divFiador_1'>
-						</div>
-					</div>
+					<!-- FIM ÁREA FIADOR -->
 					<div class="col-md-12">
 						<hr>
 					</div>
-					<!--FIM ÁREA FIADOR-->
 					<div class="col-md-3">
 						<div class="form-group">
 							{{ Form::label('survey_inspetor_name', 'Nome do vistoriador') }}
@@ -117,7 +103,6 @@
 								@include('survey.type_immobile')
 								@endif	
 							</select>
-
 						</div>
 					</div>
 					<div class="col-md-2">
@@ -157,189 +142,20 @@
 							{{ Form::text('survey_gas_load', (!empty($survey[0]->survey_gas_load)) ? $survey[0]->survey_gas_load : $survey->survey_gas_load, ['class' => 'form-control' , 'id' => 'survey_gas_load' , 'placeholder' => 'Leitura do gás'])  }} 
 						</div>
 					</div>
-
-
-				</div><!-- FIM BOX BODY -->
+				</div>
 			</div>
-		</div>
-		<!-- /.col -->
-	</div>
-	<!-- /.row -->
 
-	<div class="row">
-		<div class="col-md-12">
 			<div class="box box-primary">
 				<div class="box-header">
 					<h3 class="box-title">Aspectos Gerais
-						<small>Descreva os detalhes de todos os ambientes do imóvel
-						</small>
+						<small>Descreva os detalhes de todos os ambientes do imóvel</small>
 					</h3>
-					<!-- tools box -->
-					<div class="pull-right box-tools">
-						<button type="button" class="btn btn-primary btn-sm" data-widget="collapse" data-toggle="tooltip" title="Esconder e Mostrar">
-							<i class="fa fa-minus">
-							</i>
-						</button>
-					</div>
-					<!-- /. tools -->
 				</div>
 				<div class="box-body pad">
-
-					<textarea id="editor_aspect" name="survey_general_aspects" rows="10" cols="80">
-						@if(empty( $survey_update[0]->survey_general_aspects))
-						<p><strong>Caracter&iacute;sticas do im&oacute;vel</strong>: Apartamento com Sala, 3 quartos com arm&aacute;rios (sendo 1 su&iacute;te com box), banheiro social com box, cozinha com arm&aacute;rios, &aacute;rea de servi&ccedil;o. Ambientes: Sala, Quarto 01, Quarto 02, Su&iacute;te, WC Su&iacute;te, WC Social, Cozinha e &Aacute;rea de Servi&ccedil;o.</p>
-
-						<p><strong>Faxinado</strong>: Sim</p>
-
-						<p><strong>Mobiliado</strong>: N&atilde;o, mas com arm&aacute;rios fixos de madeira em todos os quartos, na cozinha e na &aacute;rea de servi&ccedil;o. &nbsp;</p>
-
-						<p><strong>Piso</strong>: Porcelanato branco, exceto nos banheiros que &eacute; cer&acirc;mica.</p>
-
-						<p><strong>Paredes</strong>: Pintura em l&aacute;tex na cor creme, exceto nos banheiros e na cozinha que &eacute; cer&acirc;mica.</p>
-
-						<p><strong>Teto</strong>: Pintura em l&aacute;tex na cor branco neve, exceto no banheiro que s&atilde;o forrados em PVC.</p>
-
-						<p><strong>Portas e Portais</strong>: Entrada da sala com 02 portas e 02 portais, sendo a primeira em alum&iacute;nio e a segunda de madeira, ambas com fechaduras; entrada da cozinha tamb&eacute;m com 02 portas e 02 portais, sendo a primeira em alum&iacute;nio e a segunda de madeira, ambas com fechaduras; portal e porta dos quartos e dos banheiros com porta modelo sanfonada em PVC, todas com trava.</p>
-
-						<p><strong>Instala&ccedil;&atilde;o hidr&aacute;ulica:</strong> Em perfeito funcionamento, sem ressalvas.</p>
-
-						<p><strong>Instala&ccedil;&atilde;o el&eacute;trica:</strong> Em perfeito funcionamento, sem ressalvas.</p>
-						@else 
-						{!! $survey_update[0]->survey_general_aspects !!}
-
-						@endif
-					</textarea>
-				</div>
+					<survey-content></survey-content>
+				</div>	
 			</div>
 
-		</div>
-	</div>
-	<!-- /.row aspectos gerais -->
-	<div class="row">
-		<div class="col-md-12">
-			<div class="box box-primary">
-				<div class="box-header">
-					<h3 class="box-title">Ressalvas por Ambientes / Cômodos
-					</h3>
-					<!-- tools box -->
-					<div class="pull-right box-tools">
-						<button type="button" class="btn btn-primary btn-sm" data-widget="collapse" data-toggle="tooltip" title="Maximizar / Minimizar">
-							<i class="fa fa-minus">
-							</i>
-						</button>
-					</div>
-					<!-- /. tools -->
-				</div>
-				<div class="box-body pad">
-					<textarea id="editor_reservation" name="survey_reservation" rows="10" cols="80">
-						@if(empty( $survey_update[0]->survey_reservation))
-						<p>No imóvel vistoriado foram constatadas as seguintes ressalvas:</p>
-						<strong>Sala</strong>
-						<ol>
-							<li>Fechadura com dificuldade para fechar;</li>
-						</ol>
-
-						<strong>Quarto 01</strong>
-						<ol>
-							<li>Faltando trava da porta sanfonada de PVC;</li>
-						</ol>
-						<strong>Quarto 02</strong>
-						<ol>
-							<li>Trava da porta sanfonada de PVC com dificuldade para fechar;</li>
-						</ol>
-
-						<strong>Área de serviço</strong>
-						<ol>
-							<li>Algumas cerâmicas danificadas, conforme registros fotográficos;</li>
-							<li>Armário da lavanderia com porta desregulada.</li>
-						</ol>
-						@else 
-						{!! $survey_update[0]->survey_reservation !!}
-						@endif
-					</textarea>
-				</div>
-			</div>
-		</div>
-	</div><!-- /.row Ressalvas por Ambientes / Cômodos-->
-	<div class="row">
-		<div class="col-md-12">
-			<div class="box box-primary">
-				<div class="box-header">
-					<h3 class="box-title">Disposições Gerais
-					</h3>
-					<!-- tools box -->
-					<div class="pull-right box-tools">
-						<button type="button" class="btn btn-primary btn-sm" data-widget="collapse" data-toggle="tooltip" title="Maximizar / Minimizar">
-							<i class="fa fa-minus">
-							</i>
-						</button>
-					</div>
-					<!-- /. tools -->
-				</div>
-				<div class="box-body pad">
-					<textarea id="editor_provisions" name="survey_provisions" rows="10" cols="80">
-						@if(empty( $survey_update[0]->survey_provisions))
-						<p><strong>VII&nbsp;-&nbsp;</strong>O im&oacute;vel possui condi&ccedil;&otilde;es de uso e habitabilidade, e se encontra em perfeito estado de conserva&ccedil;&atilde;o com tudo funcionando,&nbsp;<ins><strong>ressalvado&nbsp;</strong>eventuais defeitos existentes que est&atilde;o expressos no presente termo.</ins></p>
-
-						<p><strong>VIII - O</strong>&nbsp;Tour Virtual, as imagens comuns e as em 360&ordm; est&atilde;o hospedadas no dom&iacute;nio&nbsp;<a href="http://www.espindolaimobiliaria.com.br/">www.espindolaimobiliaria.com.br</a>,&nbsp;todas publicadas at&eacute; a presente data, s&atilde;o provas digitais aceitas por todas as partes e que servir&atilde;o como fonte para esclarecer d&uacute;vidas que possam surgir ao final da loca&ccedil;&atilde;o.</p>
-
-						<p><strong>IX -&nbsp;</strong>O(s) locat&aacute;rio(s) disp&otilde;e(m) de 05 (cinco) dias &uacute;teis, a partir desta data, para contesta&ccedil;&atilde;o, por e-mail, do presente termo de vistoria, o qual s&oacute; ter&aacute; efeito ap&oacute;s reconhecimento por escrito pelo Locador(a) ou pela sua procuradora (imobili&aacute;ria).</p>
-
-						<p><strong>X -&nbsp;</strong>&Eacute; vedado ao(s) LOCAT&Aacute;RIO(S) realizar(em) qualquer tipo de furo nas cer&acirc;micas do im&oacute;vel, sob pena de ter(em) que substituir as cer&acirc;micas danificadas por novas da mesma cor e modelo.</p>
-
-						<p><strong>XI -&nbsp;</strong>Os locat&aacute;rios declaram que receberam neste ato as chaves e os demais itens m&oacute;veis acima especificados, e que est&atilde;o cientes de que o aluguel e encargos come&ccedil;am a ser cobrados a partir desta data.</p>
-						@else 
-						{!! $survey_update[0]->survey_provisions !!}
-						@endif
-					</textarea>
-				</div>
-			</div>
-		</div>
-	</div><!-- /.row Disposições Gerais-->
-	<div class="row">
-		<div class="col-md-12">
-			<div class="box box-primary">
-				<div class="box-header">
-					<h3 class="box-title">Chaves 
-						<small>(e outros itens móveis)
-						</small> 
-					</h3>
-					<!-- tools box -->
-					<div class="pull-right box-tools">
-						<button type="button" class="btn btn-primary btn-sm" data-widget="collapse" data-toggle="tooltip" title="Maximizar / Minimizar">
-							<i class="fa fa-minus">
-							</i>
-						</button>
-					</div>
-					<!-- /. tools -->
-				</div>
-				<div class="box-body pad">
-
-					<textarea id="editor_keys" name="survey_keys" rows="10" cols="80">
-						@if(empty( $survey_update[0]->survey_keys))
-						<p>Declaro(amos) ter recebido neste ato as seguintes chaves e acess&oacute;rios do im&oacute;vel:</p>
-
-						<p>- 02 Chaves da porta principal (entrada);</p>
-
-						<p>- 01 chave da su&iacute;te;</p>
-
-						<p>- 01 chave do quarto;</p>
-
-						<p>- 03 tapa ralos de pia;</p>
-
-						<p>- 01 v&aacute;lvula para ralo de pia;</p>
-
-						<p>- 01 interfone da marca IntelBr&aacute;s.</p>
-						@else 
-							{!! $survey_update[0]->survey_keys !!}
-						@endif
-					</textarea>
-				</div>
-			</div>
-		</div>
-	</div><!-- /.row Chaves-->
-	<div class="row">
-		<div class="col-md-12">
 			<div class="box box-primary">
 				<div class="box-header">
 					<h3 class="box-title">Upload do ambiente
@@ -362,38 +178,11 @@
 					</div>
 				</div>
 			</div>
-
-			<div class="box-footer">
-				<div class="col-md-6">
-					<button type="button"  id="completed_button" class="btn btn-success btn-lg btn-flat pull-left"> 
-						<i class="fa fa-check-circle" aria-hidden="true">
-						</i> Finalizar Rascunho
-					</button>
-				</div>
-
-				<div class="col-md-6">
-					<button type="button" class="btn btn-primary btn-lg pull-right btn-flat" id="save_button" > 
-						<i class="fa fa-save"> Salvar Rascunho </i> 
-					</button>
-				</div>
-				{{ Form::hidden('survey_status' , $survey_update[0]->survey_status, ['id' => 'survey_status']) }}
-				<!-- PARAMETROS -->
-				@if($title_survey == "Nova-Vistoria")				
-				{{ Form::hidden('type_survey' , $title_survey) }}
-				@elseif ($title_survey == "Editar-Vistoria")
-				{{ Form::hidden('type_survey' , $title_survey) }}
-				@elseif ($title_survey == "Replicando-Vistoria")
-				{{ Form::hidden('type_survey' , $title_survey) }}				
-				@endif
-				{{ Form::hidden('survey_id' , $survey_id) }}
-			</div>
-
 		</div>
 	</div>
-</div>
-<!-- /.row Upload do ambiente-->
+@stop
 
-<!-- /.row -->
+<!-- /Main content -->
 @include('modal.survey_ambience_upload')
 @include('modal.modal_load')
 </section>
